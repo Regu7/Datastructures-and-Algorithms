84396525e6de3c0bf28933ba100b271d10ea9984
Regu7 <89179681+Regu7@users.noreply.github.com>
2021-09-08 10:25:18 +0530

    Create Majority_element_2.py

---
+++ Array/Majority_element_2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        candidate1 = 0
+        candidate2 = -1 # should be different from candidate1
+        count1 = 0
+        count2 = 0
+        
+        for n in nums:断顺序很重要
+            if candidate1 == n:
+                count1 += 1
+            elif candidate2 == n:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = n, 1
+            elif count2 == 0:
+                candidate2, count2 = n, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+                
+        return [n for n in (candidate1, candidate2) if nums.count(n) > len(nums)//3]
