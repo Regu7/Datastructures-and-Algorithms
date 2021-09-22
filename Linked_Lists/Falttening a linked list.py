5ab441a60047e6b7a5b2c7dca31bb644f6a181d6
Regu Nanthan <89179681+Regu7@users.noreply.github.com>
2021-09-22 10:52:54 +0530

    Create Falttening a linked list.py

---
+++ Linked_Lists/Falttening a linked list.py
@@ -0,0 +1,39 @@
+
+
+'''
+
+class Node:
+    def __init__(self, d):
+        self.data=d
+        self.next=None
+        self.bottom=None
+        
+'''
+def merge(a,b):
+    temp = Node(0)
+    res = temp
+    while(a!= None and b!= None):
+        if a.data <b.data:
+            temp.bottom = a
+            temp = temp.bottom
+            a = a.bottom
+        else:
+            temp.bottom = b
+            temp = temp.bottom
+            b = b.bottom
+    if a!= None:
+        temp.bottom = a
+    else:
+        temp.bottom = b
+        
+    return res.botttom
+
+def flatten(root):
+    
+    if (root == None) or (root.next == None):
+        return root
+    
+    root.next = flatten(root.next)
+    
+    root = merge(root,root.next)
+    return root
