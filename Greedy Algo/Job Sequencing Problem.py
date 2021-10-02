80d80efaa0562f4333a53048ab3f1062d3379cdd
Regu Nanthan <89179681+Regu7@users.noreply.github.com>
2021-10-02 21:56:52 +0530

    Create Job Sequencing Problem.py

---
+++ Greedy Algo/Job Sequencing Problem.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the maximum profit and the number of jobs done.
+    # Program to find the maximum profit
+# job sequence from a given array
+# of jobs with deadlines and profits
+
+# function to schedule the jobs take 2
+# arguments array and no of jobs to schedule
+
+
+            def printJobScheduling(arr, t):
+            
+                # length of array
+                n = len(arr)
+            
+                # Sort all jobs according to
+                # decreasing order of profit
+                for i in range(n):
+                    for j in range(n - 1 - i):
+                        if arr[j][2] < arr[j + 1][2]:
+                            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            
+                # To keep track of free time slots
+                result = [False] * t
+            
+                # To store result (Sequence of jobs)
+                job = ['-1'] * t
+            
+                # Iterate through all given jobs
+                for i in range(len(arr)):
+            
+                    # Find a free slot for this job
+                    # (Note that we start from the
+                    # last possible slot)
+                    for j in range(min(t - 1, arr[i][1] - 1), -1, -1):
+            
+                        # Free slot found
+                        if result[j] is False:
+                            result[j] = True
+                            job[j] = arr[i][0]
+                            break
+            
+                # print the sequence
+                print(job)
+  
